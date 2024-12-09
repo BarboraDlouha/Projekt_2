1d0a27c7a074b3468fd4e2c60cd8682573040429
BarboraDlouha <Barbora-Dlouha@seznam.cz>
2024-12-09 13:53:34 +0100

    File_deposit

---
+++ Projekt_2.py
@@ -0,0 +1,23 @@
+# Separator 
+print(60 * "-")
+
+# Header  
+print("""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: Barbora Dlouha
+email: Barbora-Dlouha@seznam.cz
+""")
+
+# Separator
+print(60 * "-")
+
+# Listing the introductory text
+print(
+    "Hi there!", 
+    45 * "-", 
+    "I've generated a random 4 digit number for you.", 
+    "Let's play a bulls and cows game.", 
+    45 * "-", 
+    sep="\n"
+    )
